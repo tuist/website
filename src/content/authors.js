ad69a57535414e89e64a1cd3ef34ada033efc81e
Pedro Piñera <pedro@pepicrft.me>
2023-06-20 16:55:34 +0200

    Implement the blog show UI

---
+++ src/content/authors.js
@@ -0,0 +1,29 @@
+export default {
+    'pepicrft': {
+        name: 'Pedro Piñera',
+        twitter: 'https://twitter.com/pepicrft',
+        mastodon: 'https://mastodon.social/@pepicrft',
+        github: 'https://github.com/pepicrft'
+    },
+    'fortmarek': {
+        name: 'Marek Fořt',
+        twitter: 'https://twitter.com/marekfort',
+        mastodon: 'https://mastodon.social/@marekfort@mastodon.online',
+        github: 'https://github.com/fortmarek'
+    },
+    'natanrolnik': {
+        name: 'Natan Rolnik',
+        twitter: 'https://twitter.com/natanrolnik',
+        mastodon: 'https://mastodon.social/@natanrolnik',
+        github: 'https://github.com/natanrolnik'
+    },
+    'vytis': {
+        name: 'Vytis',
+        twitter: 'https://twitter.com/vytis0',
+        github: 'https://github.com/vytis'
+    },
+    'ollieatkinson': {
+        name: 'Oliver Atkinson',
+        github: 'https://github.com/ollieatkinson'
+    }
+}
