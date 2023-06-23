f96988f6d1a2b60f65011c1e7dba8f8410b308bb
Pedro Piñera <pedro@pepicrft.me>
2023-06-23 16:44:15 +0200

    Implement endpoint to generate og images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,28 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx"; 
+import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-   markdown: {
+  output: 'server',
+  adapter: netlify(),
+  markdown: {
     drafts: true,
-    shikiConfig: { theme: "css-variables" }
+    shikiConfig: {
+      theme: "css-variables"
+    }
   },
   shikiConfig: {
     wrap: true,
     skipInline: false,
-    drafts: true,
+    drafts: true
   },
-   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), image(), compress(),  sitemap(), mdx()]
+  site: "https://lexingtonthemes.com",
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), compress(), sitemap(), mdx()]
 });
