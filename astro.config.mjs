bad2cb4f82199091764fdb220e09282def899088
Pedro Piñera <pedro@pepicrft.me>
2023-06-23 16:52:01 +0200

    Not use the adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,6 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
   markdown: {
     drafts: true,
     shikiConfig: {
