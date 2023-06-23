33a331637fca76535da485b2e67248ac004d1740
Pedro Piñera <pedro@pepicrft.me>
2023-06-23 10:18:54 +0200

    Update the URL

--- src/content/metadata.js
+++ src/content/metadata.js
@@ -1,7 +1,7 @@
 const metadata = {
     title: "Tuist",
     description: "Supercharge your Xcode development workflows",
-    url: "https://next.tuist.io"
+    url: "https://tuist.io"
 }
 
 export default metadata;
