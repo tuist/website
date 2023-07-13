05210ba38a722da55c00453502dad1746b246b1d
Pedro Piñera <pedro@pepicrft.me>
2023-07-13 18:43:47 +0200

    Add sponsors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './public/logos/**/*.svg'],
   theme: {
      animation: {
         marquee: 'marquee 60s linear infinite',
