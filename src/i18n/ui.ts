f5317fbb5d27c12cde630fcd6399123cfa695ce1
Pedro <pedro@pepicrft.me>
2024-03-17 13:13:42 +0100

    Set the language in the HTML

---
+++ src/i18n/ui.ts
@@ -0,0 +1,14 @@
+import en from "./locales/en.json";
+
+export const ui = {
+  en,
+};
+
+export const languages = {
+    en: 'English',
+    ko: 'Korean',
+    zh: 'Chinese',
+    ja: 'Japanese'
+  };
+  
+export const defaultLang = 'en';
