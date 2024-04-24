35af9c0384005a1a0d7b410507305c8d74e90702
Pedro <pedro@pepicrft.me>
2024-04-24 14:15:47 +0200

    Fix build

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,7 +1,7 @@
 import { ui, defaultLang, showDefaultLang } from "./ui";
 
 export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split("/");
+  const [, lang] = url ? url.pathname.split("/") : [undefined, defaultLang];
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
 }
