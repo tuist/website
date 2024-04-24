7681cfccb7239ef5896c62972b4770932d1af93a
Pedro <pedro@pepicrft.me>
2024-04-24 13:01:55 +0200

    Finish the multilingual support

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -6,6 +6,12 @@ export function getLangFromUrl(url: URL) {
   return defaultLang;
 }
 
+export function getLangFromPath(path: String) {
+  const [, lang] = path.split("/");
+  if (lang in ui) return lang as keyof typeof ui;
+  return defaultLang;
+}
+
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof (typeof ui)[typeof defaultLang], values: Record<string, string> = {}) {
     const value = ui[lang][key] || ui[defaultLang][key];
